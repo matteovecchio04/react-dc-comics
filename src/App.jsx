88c938a6f0848b1c97164cca564bfbb8badf8b88
matteovecchio04 <matteovex@gmail.com>
2025-11-04 16:41:22 +0100

    fix import bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ export default function App() {
 
   return (
     <>
-
+      <ul className="nav justify-content-end">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import "bootstrap"
-// add bootstrap after I installed it in my terminal
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
