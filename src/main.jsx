17c3af03897a98b9c508ff60eedb8365cea72c42
matteovecchio04 <matteovex@gmail.com>
2025-11-04 16:04:05 +0100

    installed and imported bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import "bootstrap"
+// add bootstrap after I installed it in my terminal
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
