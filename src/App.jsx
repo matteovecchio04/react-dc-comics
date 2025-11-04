d3f8fed7888cd9688121c1c97e1e9fb77710748d
matteovecchio04 <matteovex@gmail.com>
2025-11-04 17:20:50 +0100

    try to link header but it doesn't work

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,10 @@
+import Header from "./components/Header.jsx"
 
 export default function App() {
 
   return (
     <>
-      <header className="bg-light">
-        <nav className="d-flex justify-content-between py-3 container">
-
-          <img src="../assets/img/dc-logo.png" alt="..." width={50} />
-
-          <ul className="nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Characters</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Comics</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Movies</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">TV</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Games</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Collectibles</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Videos</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Fans</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">News</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Shop</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
+      <Header />
     </>
   )
 }
