2437d779ca7ad86fa6193ddf1cb61fd139af4843
matteovecchio04 <matteovex@gmail.com>
2025-11-05 02:00:26 +0100

    link footer.jsx to app.jsx successfully. End exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import Header from "./components/Header.jsx"
 import Main from "./components/Main.jsx"
+import Footer from "./components/Footer.jsx"
 
 export default function App() {
   return (
@@ -9,64 +10,7 @@ export default function App() {
 
       <Main />
 
-      <section id="footer-bg">
-        <div id="jumbo-footer" className="container d-flex text-secondary pt-4">
-          <ul className="list-unstyled">
-            <h5>DC COMICS</h5>
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movies</li>
-            <li>TV</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
-
-            <br />
-
-            <h5>SHOP</h5>
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
-          </ul>
-
-          <ul className="list-unstyled">
-            <h5>DC</h5>
-            <li>Terms Of Use</li>
-            <li>Privacy policy New</li>
-            <li>Ad Choices</li>
-            <li>Adverising</li>
-            <li>Jobs</li>
-            <li>Subscriptions</li>
-            <li>Talent Workshops</li>
-            <li>CPSC Certificates</li>
-            <li>Ratings</li>
-            <li>Shop Help</li>
-            <li>Contact Us</li>
-          </ul>
-
-          <ul className="list-unstyled">
-            <h5>SITES</h5>
-            <li>DC</li>
-            <li>MAD Magazine</li>
-            <li>DC Kids</li>
-            <li>DC Universe</li>
-            <li>DC Power Visa</li>
-          </ul>
-        </div>
-      </section>
-
-      <section className="footer-bg-col">
-        <div className="container py-4 d-flex justify-content-between">
-          <button className="btn btn-secondary">SIGN UP NOW!</button>
-          <div id="footer-nav">
-            <p className="text-primary">FOLLOW US</p>
-            <img src="../assets/img/footer-facebook.png" alt="..." width={40} />
-            <img src="../assets/img/footer-periscope.png" alt="..." width={40} />
-            <img src="../assets/img/footer-pinterest.png" alt="..." width={40} />
-            <img src="../assets/img/footer-twitter.png" alt="..." width={40} />
-            <img src="../assets/img/footer-youtube.png" alt="..." width={40} />
-          </div>
-        </div>
-      </section>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,64 @@
+export default function Footer() {
+    return (<footer>
+
+        <section id="footer-bg">
+            <div id="jumbo-footer" className="container d-flex text-secondary pt-4">
+                <ul className="list-unstyled">
+                    <h5>DC COMICS</h5>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+
+                    <br />
+
+                    <h5>SHOP</h5>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+
+                <ul className="list-unstyled">
+                    <h5>DC</h5>
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy New</li>
+                    <li>Ad Choices</li>
+                    <li>Adverising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+
+                <ul className="list-unstyled">
+                    <h5>SITES</h5>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </section>
+
+        <section className="footer-bg-col">
+            <div className="container py-4 d-flex justify-content-between">
+                <button className="btn btn-secondary">SIGN UP NOW!</button>
+                <div id="footer-nav">
+                    <p className="text-primary">FOLLOW US</p>
+                    <img src="../assets/img/footer-facebook.png" alt="..." width={40} />
+                    <img src="../assets/img/footer-periscope.png" alt="..." width={40} />
+                    <img src="../assets/img/footer-pinterest.png" alt="..." width={40} />
+                    <img src="../assets/img/footer-twitter.png" alt="..." width={40} />
+                    <img src="../assets/img/footer-youtube.png" alt="..." width={40} />
+                </div>
+            </div>
+        </section>
+
+    </footer>)
+}
